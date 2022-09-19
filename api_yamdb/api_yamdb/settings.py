1423a1d3b2989810690866947c9c713dcdd8534e
Красильников Виталий <vitalces@yandex.ru>
2022-09-19 15:43:45 +0500

    Коммит 6. Проверка работы workflow. Тесты flake8. 19.09.2022

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('DJANGO_KEY', 'some_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True#False
+DEBUG = True  # False
 
 ALLOWED_HOSTS = ['*']
 
